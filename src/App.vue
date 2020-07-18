16004fc8424608b33067a41e7edc0ae35d2224bf
Ben Dowen <ben@dowen.me.uk>
2020-07-18 01:46:59 +0100

    Gif weather UI

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,75 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>GIF Weather</h1>
+    <p>Enter a location to see a GIF for the current weather. For example try London.</p>
+    <input v-model="searchTerm" type="text">
+    <button class="button" @click=getGifs()>Get my Weather GIF!</button>
+    <div class="gif-container">
+      <img v-for="gif in gifs" :src="gif" :key="gif.id">
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      searchTerm: "",
+      gifs: []
+    };
+  },
+  methods: {
+    getGifs() {
+      let endpoint = "http://localhost:8000";
+      let url = `${endpoint}/${
+        this.searchTerm
+      }`;
+      fetch(url)
+        .then(response => {
+          return response.json();
+        })
+        .then(json => {
+          this.buildGifs(json);
+        })
+        .catch(err => console.log(err));
+    },
+    buildGifs(json) {
+      this.gifs = json.data.map(gif => gif.id).map(gifId => {
+        return `https://media.giphy.com/media/${gifId}/giphy.gif`;
+      });
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+input {
+  padding: 5px;
+  margin-bottom: 20px;
+}
+.button {
+  background-color: rgb(0, 172, 0);
+  color: white;
+  padding: 5px 20px;
+  border: none;
+  display: block;
+  margin: 0 auto;
+}
+.button:hover {
+  background-color: rgb(0, 148, 0);
+}
+.gif-container {
+  margin-top: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
